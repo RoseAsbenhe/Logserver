00862b2a6fa0caf49838ced332208c84c01a3fd9
vanessaboehner <boehner.vanessa@gmail.com>
2015-09-17 21:37:04 +0200

    initial commit

---
+++ routes/log.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/', function(req, res, next) {
+    console.log(req);
+    // Set our internal DB variable
+    var db = req.db;
+    var reqBody = req.body;
+
+    // Set our collection
+    var collection = db.get('usercollection');
+
+    // Submit to the DB
+    collection.insert({
+        "pluginName" : reqBody.pluginName,
+        "userid" : reqBody.userid,
+        "url" : reqBody.url,
+        "sslStatus" : reqBody.sslStatus,
+        "theme" : reqBody.theme,
+        "trust" : reqBody.trust,
+        "text" : reqBody.text
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // And forward to success page
+            res.send("success");
+        }
+    });
+});
+
+module.exports = router;
